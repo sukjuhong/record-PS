4f7d25f3c2d492ed609acf1b386b4ba018415b32
Sukju Hong <ehensnfl@gmail.com>
2023-04-16 20:21:45 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <memory.h>
+using namespace std;
+
+void Solve() {
+    string s;
+    cin >> s;
+    cout << s.size() << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    Solve();
+}
