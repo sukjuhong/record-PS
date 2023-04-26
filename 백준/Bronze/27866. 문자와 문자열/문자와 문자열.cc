586efb42909a6eddab615d77a6c40ee2be93e2ef
Sukju Hong <ehensnfl@gmail.com>
2023-04-26 21:14:48 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <memory.h>
+#include <queue>
+#include <stack>
+#include <vector>
+using namespace std;
+
+void Solve() {
+    string s;
+    int i;
+    cin >> s >> i;
+
+    cout << s[i - 1] << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    Solve();
+}
