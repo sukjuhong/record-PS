1ad2eb03abc1bc36ca27c7516927ff67a9a79acb
Sukju Hong <ehensnfl@gmail.com>
2023-10-25 18:12:07 +0900

    [Platinum V] Title: 샤워실 바닥 깔기 (Large), Time: 0 ms, Memory: 2272 KB -BaekjoonHub

--- 백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.cc
+++ 백준/Platinum/14601. 샤워실 바닥 깔기 （Large）/샤워실 바닥 깔기 （Large）.cc
@@ -1,66 +1,65 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <memory.h>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
-int tile_index;
-int arr[129][129];
+#define MAX 129
 
-bool is_filled(int y, int x, int size) {
-    for (int i=0; i<size; i++) {
-        for (int j=0; j<size; j++) {
-            if (arr[y+i][x+j] != 0) return true;
-        }
-    }
+int arr[MAX][MAX];
+int num_tile = 0;
+
+bool is_filled(int row, int col, int size)
+{
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            if (arr[row + i][col + j] != 0)
+                return true;
     return false;
 }
 
-void tiling(int y, int x, int size) {
-    int half_size = size / 2;
-    tile_index++;
+void tromino(int n, int row, int col)
+{
+    int half_n = n / 2;
+    num_tile++;
 
-    if (!is_filled(y, x, half_size)) arr[y+half_size-1][x+half_size-1] = tile_index;
-    if (!is_filled(y+half_size, x, half_size)) arr[y+half_size][x+half_size-1] = tile_index;
-    if (!is_filled(y, x+half_size, half_size)) arr[y+half_size-1][x+half_size] = tile_index;
-    if (!is_filled(y+half_size, x+half_size, half_size)) arr[y+half_size][x+half_size] = tile_index;
-    
-    if (size== 2) return;
+    if (!is_filled(row, col, half_n))
+        arr[row + half_n - 1][col + half_n - 1] = num_tile;
+    if (!is_filled(row + half_n, col, half_n))
+        arr[row + half_n][col + half_n - 1] = num_tile;
+    if (!is_filled(row, col + half_n, half_n))
+        arr[row + half_n - 1][col + half_n] = num_tile;
+    if (!is_filled(row + half_n, col + half_n, half_n))
+        arr[row + half_n][col + half_n] = num_tile;
 
-    tiling(y, x, half_size);
-    tiling(y+half_size, x, half_size);
-    tiling(y, x+half_size, half_size);
-    tiling(y+half_size, x+half_size, half_size);
+    if (n == 2)
+        return;
+
+    tromino(half_n, row, col);
+    tromino(half_n, row + half_n, col);
+    tromino(half_n, row, col + half_n);
+    tromino(half_n, row + half_n, col + half_n);
 }
 
-void solve() {
-    int n;
-    cin >> n;
-    pair<int, int> sink;
-    cin >> sink.second >> sink.first;
-
-    int arr_size;
-    arr_size = pow(2, n);
-
-    arr[sink.first][sink.second] = -1;
-
-    tiling(1, 1, arr_size);
-
-    for (int i=arr_size; i>=1; i--) {
-        for (int j=1; j<=arr_size; j++) {
-            if (j != 1) cout << " ";
-            cout << arr[i][j];
-        }
-        cout << endl;
-    }
-}
-
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    solve();
+    int k;
+    cin >> k;
+
+    int n = pow(2, k);
+
+    int x, y;
+    cin >> x >> y;
+    arr[y][x] = -1;
+
+    tromino(n, 1, 1);
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= n; j++)
+            cout << arr[i][j] << " ";
+        cout << "\n";
+    }
     return 0;
 }
