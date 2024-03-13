6d693e302867cbcacd04c0201c1fb3bd081cd419
SukJu Hong <ehensnfl@gmail.com>
2024-03-13 11:53:54 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,75 +1,72 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <memory.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int parent[10001];
-
-int Find(int x) {
-    if (x == parent[x]) return x;
-    return parent[x] = Find(parent[x]);
-}
-
-bool Union(int x, int y) {
-    int px = Find(x);
-    int py = Find(y);
-    if (px != py) {
-        parent[py] = px;
-        return true;
-    }
-    return false;
-}
-
 struct Edge {
-    int l;
-    int r;
-    int weight;
+  int l;
+  int r;
+  int w;
 };
 
-struct Comp {
-    bool operator()(Edge a, Edge b) {
-        return a.weight > b.weight;
-    }
-};
-
-void Solve() {
-    int v, e;
-    cin >> v >> e;
-
-    priority_queue<Edge, vector<Edge>, Comp> pq;
-    for (int i=0; i<e; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        pq.push({a, b, c});
-    }
-
-    for (int i=1; i<=v; i++) {
-        parent[i] = i;
-    }
-
-    int num_edge = 0;
-    int sum_weight = 0;
-    while (true) {
-        Edge edge = pq.top();
-        pq.pop();
-
-        if (Union(edge.l, edge.r)) {
-            num_edge++;
-            sum_weight += edge.weight;
-        }
-
-        if (num_edge == v-1) break;
-    }
-
-    cout << sum_weight << "\n";
+bool comp(Edge& l, Edge& r) {
+  return l.w < r.w;
 }
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+vector<Edge> edges;
+int parents[10001];
 
-    Solve();
-    return 0;
+int union_find(int x) {
+  if (x == parents[x]) return x;
+  return parents[x] = union_find(parents[x]);
+}
+
+bool union_merge(int l, int r) {
+  int pl = union_find(l);
+  int pr = union_find(r);
+  if (pl != pr) {
+    parents[pr] = pl;
+    return true;
+  }
+  return false;
+}
+
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int v, e;
+  cin >> v >> e;
+
+  for (int i=1; i<=v; i++) {
+    parents[i] = i;
+  }
+
+  for (int i=0; i<e; i++) {
+    int l, r, w;
+    cin >> l >> r >> w;
+    edges.push_back({l, r, w});
+  }
+
+  sort(edges.begin(), edges.end(), comp);
+
+  int sum_weight = 0;
+  int num_edges = 0;
+  for (int i=0; i<edges.size(); i++) {
+    int l = edges[i].l;
+    int r = edges[i].r;
+
+    if (union_merge(l, r)) {
+      sum_weight += edges[i].w;
+      num_edges++;
+    }
+
+    if (num_edges == v-1) {
+      break;
+    }
+  }
+
+  cout << sum_weight << "\n";
+  return 0;
 }
