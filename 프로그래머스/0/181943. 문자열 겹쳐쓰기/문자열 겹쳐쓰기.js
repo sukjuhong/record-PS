577190886f49eca851bd3ac650ab27ffceef9e1f
SukJu Hong <ehensnfl@gmail.com>
2025-02-18 09:04:34 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,5 @@
+function solution(my_string, overwrite_string, s) {
+    return my_string.substring(0, s) +
+        overwrite_string +
+        my_string.substring(s + overwrite_string.length, my_string.length);
+}
