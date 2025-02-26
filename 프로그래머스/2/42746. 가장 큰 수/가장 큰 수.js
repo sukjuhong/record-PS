db331adea07ac3a3f6014b14663f9d6ee8ee06b9
SukJu Hong <ehensnfl@gmail.com>
2025-02-26 14:50:15 +0900

    [level 2] Title: 가장 큰 수, Time: 187.12 ms, Memory: 45.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+    const result = numbers
+        .map(String)
+        .sort((a, b) => (b + a) - (a + b))
+    	.join("")
+   	return result[0] === "0" ? "0" : result
+}
