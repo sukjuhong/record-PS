574bcf033678c37024e1c6f3aef4a5e1eac219d3
SukJu Hong <ehensnfl@gmail.com>
2025-03-03 12:54:40 +0900

    [level 3] Title: 정수 삼각형, Time: 9.57 ms, Memory: 42.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,17 @@
+function solution(triangle) {
+    const n = triangle.length
+    
+    const memo = Array.from(new Array(n), () => new Array(n).fill(0))
+    
+    return dp(triangle, memo, n, 0, 0)
+}
+
+function dp(triangle, memo, size, i, j) {
+   if (size <= i || size <= j) return 0
+    
+   if (memo[i][j] !== 0) return memo[i][j]
+    
+   const result = triangle[i][j] + Math.max(dp(triangle, memo, size, i+1, j), dp(triangle, memo, size, i+1, j+1));
+   memo[i][j] = result;
+    return result;
+}
