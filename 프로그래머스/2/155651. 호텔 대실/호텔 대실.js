9d97148e62b8d3cc451c561cf4b99f40d76fb1a8
SukJu Hong <ehensnfl@gmail.com>
2025-04-16 16:51:46 +0900

    [level 2] Title: 호텔 대실, Time: 9.32 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -1,26 +1,27 @@
 function solution(book_time) {
     function timeToNum(time) {
-        const [hour, minute] = time
-        	.split(":")
-        return parseInt(hour) * 60 + parseInt(minute);
+        const [hour, minute] = time.split(":").map(Number)
+        return hour * 60 + minute;
     }
     
     const bookTimes = book_time
-    	.map(row => [timeToNum(row[0]), timeToNum(row[1])])
-    	.map(row => [row[0], row[1] + 10])
-    	.sort((a, b) => a[1] - b[1]);
+    	.map(row => [timeToNum(row[0]), timeToNum(row[1]) + 10])
+    	.sort((a, b) => a[0] - b[0]);
    
-    let maxCnt = 1;
-    for (let i=0; i<bookTimes.length; i++) {
-        let cnt = 1;
-        for (let j=i+1; j<bookTimes.length; j++) {
-            if (bookTimes[i][0] < bookTimes[j][1] &&
-               bookTimes[i][1] > bookTimes[j][0]) {
-                cnt++;
+    let maxRooms = 0;
+    const rooms = [];
+
+    for (const [start, end] of bookTimes) {
+        // 현재 예약 시작 전에 끝난 방 제거
+        for (let i = 0; i < rooms.length; i++) {
+            if (rooms[i] <= start) {
+                rooms.splice(i, 1);
+                i--; // 방 번호 밀림 방지
             }
         }
-        maxCnt = Math.max(maxCnt, cnt);
+        rooms.push(end); // 새로운 방 사용
+        maxRooms = Math.max(maxRooms, rooms.length);
     }
     
-    return maxCnt;
+    return maxRooms;
 }
